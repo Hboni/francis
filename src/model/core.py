0636d8474a7d348449dd5e3bcaa77bd3605edc8b
jean unrug <jeanunrug@gmail.com>
2021-03-15 18:26:42 +0100

    adapt model for 3d morpho

--- src/model/core.py
+++ src/model/core.py
@@ -3,19 +3,24 @@ import copy
 import numpy as np
 
 def erode(im, size):
-    eroded = copy.copy(im)
-    if size > 1:
-        for i in range(len(im)):
-            eroded[i] = morphology.erosion(im[i], morphology.square(size))
+    if size == 0:
+        return im
+    if len(im.shape) == 3:
+        selem = morphology.ball(size)
+    elif len(im.shape) == 2:
+        selem = morphology.disk(size)
+    eroded = morphology.erosion(im, selem)
     return eroded
 
 def dilate(im, size):
-    dilated = copy.copy(im)
-    if size > 1:
-        for i in range(len(im)):
-            dilated[i] = morphology.dilation(im[i], morphology.square(size))
+    if size == 0:
+        return im
+    if len(im.shape) == 3:
+        selem = morphology.ball(size)
+    elif len(im.shape) == 2:
+        selem = morphology.disk(size)
+    dilated = morphology.dilation(im, selem)
     return dilated
-
 
 def applyThreshold(im, threshold, reverse=False):
     mask = np.zeros_like(im)
