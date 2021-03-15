2629de06907fc9acdf923feeda703399d8fca8e5
jean unrug <jeanunrug@gmail.com>
2021-03-15 19:20:29 +0100

    complete and comment erosion/dilation models

--- src/model/core.py
+++ src/model/core.py
@@ -2,23 +2,48 @@ from skimage import morphology
 import copy
 import numpy as np
 
-def erode(im, size):
+def erode(im, size, round_shape=True):
+    """
+    return an eroded image
+    im: 2d or 3d array
+    size:   int:    generate an element of size (size*2+1)
+    round_shape: str:    if True the elemnt is ball or disk
+    """
     if size == 0:
         return im
     if len(im.shape) == 3:
-        selem = morphology.ball(size)
+        if round_shape:
+            selem = morphology.ball(size)
+        else:
+            selem = morphology.cube(size*2+1)
     elif len(im.shape) == 2:
-        selem = morphology.disk(size)
+        if round_shape:
+            selem = morphology.disk(size)
+        else:
+            selem = morphology.square(size*2+1)
+
     eroded = morphology.erosion(im, selem)
     return eroded
 
-def dilate(im, size):
+def dilate(im, size, round_shape=False):
+    """
+    return a dilated image
+    im: 2d or 3d array
+    round_shape: str:    if True the elemnt is ball or disk
+    size:   int:    generate an element of size (size*2+1)
+    """
     if size == 0:
         return im
     if len(im.shape) == 3:
-        selem = morphology.ball(size)
+        if round_shape:
+            selem = morphology.ball(size)
+        else:
+            selem = morphology.cube(size*2+1)
     elif len(im.shape) == 2:
-        selem = morphology.disk(size)
+        if round_shape:
+            selem = morphology.disk(size)
+        else:
+            selem = morphology.square(size*2+1)
     dilated = morphology.dilation(im, selem)
     return dilated
 
