3b57ad433d009d1ba6c97ab872c68595846248d1
jean unrug <jeanunrug@gmail.com>
2021-03-15 19:19:50 +0100

    update erosion/dilation test

--- tests/test_model.py
+++ tests/test_model.py
@@ -10,14 +10,14 @@ def square():
     sq_arr[2:8, 2:8, 2:8] = 1
     return sq_arr
 
-
 def test_erode(square):
-
-    assert np.sum(core.erode(square, 1)) == 4**3
-    assert np.sum(core.erode(square, 2)) == 2**3
-
+    assert np.sum(core.erode(square, 1, round_shape=False)) == 4**3
+    assert np.sum(core.erode(square, 2, round_shape=False)) == 2**3
+    assert np.sum(core.erode(square, 1, round_shape=True)) == 4**3
+    assert np.sum(core.erode(square, 2, round_shape=True)) == 2**3
 
 def test_dilation(square):
-
-    assert np.sum(core.dilate(square, 1)) == 8**3
-    assert np.sum(core.dilate(square, 2)) == 10**3
+    assert np.sum(core.dilate(square, 1, round_shape=False)) == 8**3
+    assert np.sum(core.dilate(square, 2, round_shape=False)) == 10**3
+    assert np.sum(core.dilate(square, 1, round_shape=True)) == 432
+    assert np.sum(core.dilate(square, 2, round_shape=True)) == 728
