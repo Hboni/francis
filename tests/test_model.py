84d66582e69ece26a1897dc58f6a178c58120a1d
Hboni <27290853+Hboni@users.noreply.github.com>
2021-03-20 10:25:23 +0100

    Add more tests for erosion/dilation (#14)

--- tests/test_model.py
+++ tests/test_model.py
@@ -4,10 +4,16 @@ from src.model import core
 
 
 @pytest.fixture
+def cube():
+    cube_arr = np.zeros((10, 10, 10))
+    cube_arr[2:8, 2:8, 2:8] = 1
+    return cube_arr
+
+
+@pytest.fixture
 def square():
-    # Initialize a sphere
-    sq_arr = np.zeros((10, 10, 10))
-    sq_arr[2:8, 2:8, 2:8] = 1
+    sq_arr = np.zeros((10, 10))
+    sq_arr[2:8, 2:8] = 1
     return sq_arr
 
 
@@ -16,34 +22,40 @@ def grey_scale():
     return np.array([[i]*10 for i in range(10)])
 
 
-def test_erode(square):
-    assert np.sum(core.erode(square, 1, round_shape=False)) == 4**3
-    assert np.sum(core.erode(square, 2, round_shape=False)) == 2**3
-    assert np.sum(core.erode(square, 1, round_shape=True)) == 4**3
-    assert np.sum(core.erode(square, 2, round_shape=True)) == 2**3
+def test_erode(cube, square):
+    assert np.sum(core.erode(cube, 1, round_shape=False)) == 4**3
+    assert np.sum(core.erode(cube, 2, round_shape=False)) == 2**3
+
+    assert np.sum(core.erode(square, 1, round_shape=True)) == 4**2
+    assert np.sum(core.erode(square, 2, round_shape=True)) == 2**2
+
+    assert np.array_equal(core.erode(square, 1, round_shape=True),
+                          core.erode(square, 1, round_shape=False))
 
     assert np.array_equal(
         core.erode(
-            core.dilate(square, 1, round_shape=False),
+            core.dilate(cube, 1, round_shape=False),
             1, round_shape=False
         ),
-        square
-    )
+        cube)
 
 
-def test_dilation(square):
-    assert np.sum(core.dilate(square, 1, round_shape=False)) == 8**3
-    assert np.sum(core.dilate(square, 2, round_shape=False)) == 10**3
-    assert np.sum(core.dilate(square, 1, round_shape=True)) == 432
-    assert np.sum(core.dilate(square, 2, round_shape=True)) == 728
+def test_dilation(cube, square):
+    assert np.sum(core.dilate(cube, 1, round_shape=False)) == 8**3
+    assert np.sum(core.dilate(cube, 2, round_shape=False)) == 10**3
+    assert np.sum(core.dilate(cube, 1, round_shape=True)) == 432
+    assert np.sum(core.dilate(cube, 2, round_shape=True)) == 728
+
+    assert np.sum(core.dilate(square, 1, round_shape=False)) == 8**2
+    assert np.sum(core.dilate(square, 2, round_shape=False)) == 10**2
+    assert np.sum(core.dilate(square, 1, round_shape=True)) == 8**2 - 4
 
     assert np.array_equal(
         core.dilate(
-            core.erode(square, 1, round_shape=False),
+            core.erode(cube, 1, round_shape=False),
             1, round_shape=False
         ),
-        square
-    )
+        cube)
 
 
 def test_threshold(grey_scale):
