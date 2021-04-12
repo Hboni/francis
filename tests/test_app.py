8cd2b4961bcfd78b32209caf88452f0f5980f790
Hboni <27290853+Hboni@users.noreply.github.com>
2021-04-12 22:24:19 +0200

    Add more testsfor interface

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,26 @@
 # Test of basic application usage
 
 import pytest
+import os
+
 from src.view.app import MainWindow
+from src.controller.connection import Connector
 from PyQt5 import QtCore
+
+
+@pytest.fixture
+def test_image_path():
+    return os.path.join(os.getcwd(),
+                        'data/cvs_avg35_inMNI152.nii.gz')
 
 
 @pytest.fixture
 def francis():
     # Launch Francis
-    return MainWindow()
+    win = MainWindow()
+    conn = Connector()
+    conn.connect_view_to_model(win)
+    return win
 
 
 def test_francis_launch(qtbot, francis):
@@ -16,8 +28,34 @@ def test_francis_launch(qtbot, francis):
     # win.show()
     qtbot.addWidget(francis)
 
+    assert not francis.graph.nodes
 
-def test_load_image(qtbot, francis):
-    # win = MainWindow()
+
+def test_load_image(qtbot, francis, test_image_path):
+    """Select load image node and load the demonstration image"""
+    load_image_node = 'load image'
     qtbot.addWidget(francis)
-    qtbot.mouseClick(francis, QtCore.Qt.RightButton)
+    francis.graph.addNode(load_image_node)
+
+    # Click on Load image btn
+    with qtbot.waitSignal(francis.graph.nodeClicked,
+                          timeout=2000,
+                          raising=True):
+        qtbot.mouseClick(francis.graph.nodes[load_image_node].button,
+                         QtCore.Qt.LeftButton)
+    # Fill the widget with the image path
+    assert os.path.exists(test_image_path)
+    francis.graph.nodes[load_image_node].path = test_image_path
+    # Click on apply
+    qtbot.mouseClick(francis.graph.nodes[load_image_node].parameters.itemAt(0).widgets.apply,
+                     QtCore.Qt.LeftButton)
+
+    assert len(francis.graph.nodes) == 1
+
+
+def test_add_nodes(qtbot, francis):
+    qtbot.addWidget(francis)
+    francis.graph.addNode('multiply image')
+    assert len(francis.graph.nodes) == 1
+    francis.graph.addNode('load image')
+    assert len(francis.graph.nodes) == 2
